9f7217fcb4cc8f143f9236e081cf8b2f2ffd821c
21120508 <ladykillal1232@gmail.com>
2023-12-20 02:34:45 +0700

    [server] update invoice schema

--- server/models/Invoice.js
+++ server/models/Invoice.js
@@ -21,5 +21,21 @@ const invoiceSchema = new mongoose.Schema({
   },
 });
 
+invoiceSchema.pre("save", async function (next) {
+  try {
+    const order = await Order.findById(this.order);
+    const coupon = await Coupon.findById(this.couponUsed);
+    if (coupon) {
+      this.totalMoney =
+        order.totalMoney - (coupon.discountValue * order.totalMoney) / 100;
+      coupon.timeUsed += 1;
+      await coupon.save();
+    } else this.totalMoney = order.totalMoney;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
 const Invoice = mongoose.model("Invoice", invoiceSchema);
 module.exports = Invoice;
