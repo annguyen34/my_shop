2f800b389f35a8ff2133c19fe4664a648ae253c7
21120508 <ladykillal1232@gmail.com>
2023-12-20 01:57:00 +0700

    [server] Add product schema

---
+++ server/models/Product.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter product name"],
+    trim: true,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter product price"],
+    maxLength: [8, "Price cannot exceed 8 characters"],
+    default: 0,
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter product description"],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+      },
+      url: {
+        type: String,
+      },
+    },
+  ],
+  discount: {
+    type: String,
+    min: [0, "Discount must bigger than 0"],
+    max: [100, "Discount must smaller than 100"],
+  },
+});
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
